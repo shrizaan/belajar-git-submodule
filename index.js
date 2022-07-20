1d5b19ee1d760cca7c4fb386d7692717327ab142
Shahrizan <shahrizan.learner@gmail.com>
2022-07-20 08:18:27 +0700

    update submodule

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const {sayHello, makePersonObj} = require('./library.js');
+const {sayHello, makePersonObj, sum} = require('./library.js');
 const obj1 = makePersonObj('John', 30);
 const result = sayHello("John");
+const result2 = sum(1, 2);
 console.log(result);
+console.log(result2);
 console.log(obj1);
--- library.js
+++ library.js
@@ -1,4 +1,5 @@
 const sayHello = (name) => `Hello ${name}`;
 const makePersonObj = (name, age) => ({name, age});
+const sum = (a, b) => a + b;
 
-module.exports = { sayHello, makePersonObj };
+module.exports = { sayHello, makePersonObj, sum };
